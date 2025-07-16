6f2beac9d3f3a14d80ee3e3111097f5acf341262
Douglas Lacerda <douglas.lacerda@bcrservicos.com.br>
2025-07-16 14:52:13 -0300

    wip

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(_ => ({
-    plugins: [react()],
-    build: {
-        lib: {
-            name: 'React19 Loading',
-            formats: ['es'],
-            entry: [
-                resolve(__dirname, 'lib', 'react-loading.jsx'),
-            ],
-            fileName: (format, entryName) => `${entryName}.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react'],
-        },
+  plugins: [react()],
+  build: {
+    lib: {
+      name: 'React Loading',
+      entry:  resolve(__dirname, 'lib', 'react-loading.jsx'),
+      formats: ['es'],
+      fileName: _ => `react-loading.jsx`,
     },
+    rollupOptions: {
+      external: ['react'],
+    },
+  },
 }))
